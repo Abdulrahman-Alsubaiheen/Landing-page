e74384b6d73891ec44d887efd13a93985688d78b
aboXmsa3d <aboXmsa3d@outlook.com>
2020-11-17 07:20:33 +0300

    Active the sections while scrolling.

--- js/app.js
+++ js/app.js
@@ -21,30 +21,31 @@
 const sections = document.querySelectorAll('section');
 const navbar_list = document.querySelector('#navbar__list');
 
-
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
-for (let i = 0; i < sections.length; i++) {
+function remove_active_class() {
+  for (let i = 0; i < sections.length; i++) {
     let item = sections[i];
-
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-
-    a.textContent = item.dataset.nav;   // HTML data-* Attribute
-
-    a.setAttribute("href", '#' + item.getAttribute('id'));
-
-    li.classList.add("menu__link");
-
-    li.appendChild(a);
-    navbar_list.appendChild(li);
-
-    console.log(item);
+  
+    item.classList.remove("your-active-class");
+  
   }
+}
+
+// need to call
+function rect_class() {
+  for (let i = 0; i < sections.length; i++) {
+    let item = sections[i];
+  
+    let rect = item.getBoundingClientRect();
+    console.log(rect);
+  }
+}
+
 
 /**
  * End Helper Functions
@@ -54,12 +55,42 @@ for (let i = 0; i < sections.length; i++) {
 
 // build the nav
 
+function build_nav_bar(){
+  for (let i = 0; i < sections.length; i++) {
+    let item = sections[i];
+
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.textContent = item.dataset.nav;   // HTML data-* Attribute
+    a.setAttribute("href", '#' + item.getAttribute('id'));
+
+    li.classList.add("menu__link");
+    li.appendChild(a);
+
+    navbar_list.appendChild(li);
+
+    // console.log(item);
+  }
+}
 
 // Add class 'active' to section when near top of viewport
 
+function active_class(){
+  for (let i = 0; i < sections.length; i++) {
+    let item = sections[i];
+    let rect = item.getBoundingClientRect();
 
-// Scroll to anchor ID using scrollTO event
-
+    if (rect.top < 250 && rect.bottom > 250) {
+      if (!item.classList.contains('your-active-class')){
+        remove_active_class();
+        item.classList.add("your-active-class");
+        // console.log(item);
+      }
+    };
+ 
+  }
+}
 
 /**
  * End Main Functions
@@ -68,9 +99,8 @@ for (let i = 0; i < sections.length; i++) {
 */
 
 // Build menu 
-
-// Scroll to section on link click
+build_nav_bar()
 
 // Set sections as active
-
+window.addEventListener("scroll", active_class);
 
