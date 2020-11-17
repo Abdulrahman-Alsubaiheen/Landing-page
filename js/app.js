113f03d73c23a1c900a1d7dfc4dde66f46f69177
aboXmsa3d <aboXmsa3d@outlook.com>
2020-11-17 07:33:14 +0300

    Format the Code.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,22 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
-const sections = document.querySelectorAll('section');
-const navbar_list = document.querySelector('#navbar__list');
+const sections = document.querySelectorAll("section");
+const navbar_list = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
 function remove_active_class() {
   for (let i = 0; i < sections.length; i++) {
     let item = sections[i];
-  
+
     item.classList.remove("your-active-class");
-  
   }
 }
 
@@ -40,30 +24,28 @@ function remove_active_class() {
 function rect_class() {
   for (let i = 0; i < sections.length; i++) {
     let item = sections[i];
-  
+
     let rect = item.getBoundingClientRect();
     console.log(rect);
   }
 }
 
-
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
-function build_nav_bar(){
+function build_nav_bar() {
   for (let i = 0; i < sections.length; i++) {
     let item = sections[i];
 
-    const li = document.createElement('li');
-    const a = document.createElement('a');
+    const li = document.createElement("li");
+    const a = document.createElement("a");
 
-    a.textContent = item.dataset.nav;   // HTML data-* Attribute
-    a.setAttribute("href", '#' + item.getAttribute('id'));
+    a.textContent = item.dataset.nav; // HTML data-* Attribute
+    a.setAttribute("href", "#" + item.getAttribute("id"));
 
     li.classList.add("menu__link");
     li.appendChild(a);
@@ -75,32 +57,30 @@ function build_nav_bar(){
 }
 
 // Add class 'active' to section when near top of viewport
-
-function active_class(){
+function active_class() {
   for (let i = 0; i < sections.length; i++) {
     let item = sections[i];
     let rect = item.getBoundingClientRect();
 
     if (rect.top < 250 && rect.bottom > 250) {
-      if (!item.classList.contains('your-active-class')){
+      if (!item.classList.contains("your-active-class")) {
         remove_active_class();
         item.classList.add("your-active-class");
         // console.log(item);
       }
-    };
- 
+    }
   }
 }
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-build_nav_bar()
+ 
+// Build menu
+build_nav_bar();
 
 // Set sections as active
 window.addEventListener("scroll", active_class);
-
